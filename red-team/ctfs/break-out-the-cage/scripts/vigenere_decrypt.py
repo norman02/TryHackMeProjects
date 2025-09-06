7409a7e480c2ce75d7446f7928c0d6957e08d13d
John Norman <normanj777@gmail.com>
2025-09-06 11:12:48 -0400

    Feat/break out the cage (#18)
    * ignore secrets in all directory paths
    
    * Revert "ignore secrets in all directory paths"
    
    This reverts commit f2d48da6ef95256a932150e9608d9848e0f2b93c.
    
    * Revert "Revert "ignore secrets in all directory paths""
    
    This reverts commit 4d57baadb616dee6146cd298c50a3c8f3b0d8a06.
    
    * Fresh Start
    
    * feat/break-out-the-cage initial commit
    
    * feat/break-out-the-cage enumerate with gobuster
    
    * feat/break-out-the-cage enumerate for file extensions
    
    * feat/break-out-the-cage create a script to decode dad_tasks file

---
+++ red-team/ctfs/break-out-the-cage/scripts/vigenere_decrypt.py
@@ -0,0 +1,19 @@
+# vigenere_decrypt.py
+def vigenere_decrypt(ciphertext, key):
+    plaintext = 'OCTOPUS'
+    key = key.upper()
+    key_len = len(key)
+    for i, c in enumerate(ciphertext):
+        if c.isalpha():
+            shift = ord(key[i % key_len]) - ord('A')
+            base = ord('A') if c.isupper() else ord('a')
+            decrypted = chr((ord(c) - base - shift) % 26 + base)
+            plaintext += decrypted
+        else:
+            plaintext += c
+    return plaintext
+
+# Example usage
+cipher = open("loot/cipher_vigenere.txt").read()
+print(vigenere_decrypt(cipher, "WESTON"))
+
